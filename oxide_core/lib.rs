58de898c6fecb5fc0af67e76f4d6151c5d3b5b4e
BeLeap <changseo_jang@korea.ac.kr>
2021-02-03 23:21:19 +0900

    feat:get length function

--- oxide_core/lib.rs
+++ oxide_core/lib.rs
@@ -1,4 +1,14 @@
 pub struct Vector {
-    x: f64,
-    y: f64,
+    pub x: f64,
+    pub y: f64,
+}
+
+trait VectorTrait {
+    fn length(&self) -> f64;
+}
+
+impl VectorTrait for Vector {
+    fn length(&self) -> f64 {
+        return self.x * self.x + self.y * self.y;
+    }
 }
