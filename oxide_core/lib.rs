98a72e6eaefe05169907eb8e6a4c4cc9b59daeca
BeLeap <changseo_jang@korea.ac.kr>
2021-02-03 23:14:56 +0900

    feat:define Vector struct

--- oxide_core/lib.rs
+++ oxide_core/lib.rs
@@ -1,12 +1,4 @@
-pub fn add(x: i32, y: i32) -> i32 {
-    x + y
-}
-
-#[cfg(test)]
-mod test {
-    #[test]
-    fn test_add() {
-        use super::*;
-        assert_eq!(add(3, 4), 7);
-    }
+pub struct Vector {
+    x: f64,
+    y: f64,
 }
