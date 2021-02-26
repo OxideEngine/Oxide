690e6343dac1048672aa580e82b5a901d28bf09b
BeLeap <changseo_jang@korea.ac.kr>
2021-02-27 03:14:52 +0900

    feat:add outer product

--- oxide_core/vector.rs
+++ oxide_core/vector.rs
@@ -5,9 +5,15 @@ pub trait Length {
 }
 
 pub trait Rotate {
+    // TODO: Can be implemented only after matrix implemented
     fn rotate(&self, rotationMatrix: matrix::Matrix) -> Self;
 }
 
 pub trait InnerProduct {
     fn inner_product(&self, _rhs: Self) -> i32;
+}
+
+pub trait OuterProduct {
+    // TODO: Can be implemented only after matrix implemented
+    fn outer_product(&self, _rhs: Self) -> matrix::Matrix;
 }
