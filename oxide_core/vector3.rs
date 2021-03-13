62c23299855ed2d47c2714fc241949a57ca22c7b
BeLeap <changseo_jang@korea.ac.kr>
2021-03-13 13:58:50 +0900

    feat:add length to vector3

--- oxide_core/vector3.rs
+++ oxide_core/vector3.rs
@@ -1,5 +1,13 @@
+use crate::vector;
+
 pub struct Vector3 {
     pub x: f32,
     pub y: f32,
     pub z: f32,
+}
+
+impl vector::Length for Vector3 {
+    fn get_length(&self) -> f32 {
+        self.x * self.x + self.y * self.y + self.z * self.z
+    }
 }
