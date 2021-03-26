5474085597db01257e149d218351078317fb0556
BeLeap <changseo_jang@korea.ac.kr>
2021-03-27 00:49:46 +0900

    feat:add vector2 get_squared_length() test

--- oxide_core/lib.rs
+++ oxide_core/lib.rs
@@ -9,8 +9,14 @@ mod test {
     use super::*;
 
     #[test]
+    fn test_vector2_get_squared_length() {
+        let new_vector = vector2::Vector2 { x: 3.0, y: 4.0 };
+        assert_eq!(25.0, new_vector.get_squared_length());
+    }
+
+    #[test]
     fn test_vector2_get_length() {
         let new_vector = vector2::Vector2 { x: 3.0, y: 4.0 };
-        assert_eq!(5.0, new_vector.get_length())
+        assert_eq!(5.0, new_vector.get_length());
     }
 }
