7c01a3d1a36c09ccb555a3f8b3a041dc2e08e637
BeLeap <changseo_jang@korea.ac.kr>
2021-03-27 00:41:33 +0900

    feat:add test for Vector2 get_length()

--- oxide_core/lib.rs
+++ oxide_core/lib.rs
@@ -5,5 +5,12 @@ pub mod vector3;
 
 #[cfg(test)]
 mod test {
+    use super::vector::*;
     use super::*;
+
+    #[test]
+    fn test_vector2_get_length() {
+        let new_vector = vector2::Vector2 { x: 3.0, y: 4.0 };
+        assert_eq!(5.0, new_vector.get_length())
+    }
 }
