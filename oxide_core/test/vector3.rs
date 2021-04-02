c48ed4614df44884460214b7ce08e64733faa5fd
BeLeap <changseo_jang@korea.ac.kr>
2021-04-02 17:28:11 +0900

    add scale_3 test for vector3

--- oxide_core/test/vector3.rs
+++ oxide_core/test/vector3.rs
@@ -39,4 +39,19 @@ mod test {
         let res = 92.0;
         assert_eq!(res, new_vector_1.inner_product(new_vector_2));
     }
+
+    #[test]
+    fn scale_3() {
+        let new_vector = vector3::Vector3 {
+            x: 3.0,
+            y: 4.0,
+            z: 5.0,
+        };
+        let res_vector = vector3::Vector3 {
+            x: 9.0,
+            y: 12.0,
+            z: 15.0,
+        };
+        assert_eq!(res_vector, new_vector.scale(3.0));
+    }
 }
