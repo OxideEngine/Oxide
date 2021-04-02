2a085c7bdf452c71ad75440f4ac3d1332cb425a6
BeLeap <changseo_jang@korea.ac.kr>
2021-04-02 17:37:47 +0900

    specify type of res in test

--- oxide_core/test/vector2.rs
+++ oxide_core/test/vector2.rs
@@ -5,20 +5,23 @@ mod test {
     #[test]
     fn get_squared_length() {
         let new_vector = vector2::Vector2 { x: 3.0, y: 4.0 };
-        assert_eq!(25.0, new_vector.get_squared_length());
+        let res: f32 = 25.0;
+        assert_eq!(res, new_vector.get_squared_length());
     }
 
     #[test]
     fn get_length() {
         let new_vector = vector2::Vector2 { x: 3.0, y: 4.0 };
-        assert_eq!(5.0, new_vector.get_length());
+        let res: f32 = 5.0;
+        assert_eq!(res, new_vector.get_length());
     }
 
     #[test]
     fn inner_product() {
         let new_vector_1 = vector2::Vector2 { x: 3.0, y: 4.0 };
         let new_vector_2 = vector2::Vector2 { x: 3.0, y: 7.0 };
-        assert_eq!(37.0, new_vector_1.inner_product(new_vector_2));
+        let res: f32 = 37.0;
+        assert_eq!(res, new_vector_1.inner_product(new_vector_2));
     }
 
     #[test]
