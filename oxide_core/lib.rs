6e3d4cff05acce116c1c2f4918dc445cfaced920
BeLeap <changseo_jang@korea.ac.kr>
2021-04-02 16:58:04 +0900

    add inner product test

--- oxide_core/lib.rs
+++ oxide_core/lib.rs
@@ -1,19 +1,2 @@
 pub mod matrix;
 pub mod vector;
-
-#[cfg(test)]
-mod test {
-    use super::vector::*;
-
-    #[test]
-    fn test_vector2_get_squared_length() {
-        let new_vector = vector2::Vector2 { x: 3.0, y: 4.0 };
-        assert_eq!(25.0, new_vector.get_squared_length());
-    }
-
-    #[test]
-    fn test_vector2_get_length() {
-        let new_vector = vector2::Vector2 { x: 3.0, y: 4.0 };
-        assert_eq!(5.0, new_vector.get_length());
-    }
-}
