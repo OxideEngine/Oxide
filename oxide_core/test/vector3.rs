fe1876d88c6ed392c564b4ee0cebcbafd1b25468
BeLeap <changseo_jang@korea.ac.kr>
2021-04-02 17:18:42 +0900

    add get length test for vector3

--- oxide_core/test/vector3.rs
+++ oxide_core/test/vector3.rs
@@ -1,5 +1,15 @@
 #[cfg(test)]
 mod test {
+    use crate::vector::*;
+
     #[test]
-    fn get_length() {}
+    fn get_length() {
+        let new_vector = vector3::Vector3 {
+            x: 3.0,
+            y: 4.0,
+            z: 5.0,
+        };
+        let res = 7.0710678118654752440084436210485;
+        assert_eq!(res, new_vector.get_length());
+    }
 }
