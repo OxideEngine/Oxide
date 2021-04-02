610dce2d98209720afe64bbc8677c8684057e22a
BeLeap <changseo_jang@korea.ac.kr>
2021-04-02 18:35:30 +0900

    use approx eq

--- oxide_core/test/utils.rs
+++ oxide_core/test/utils.rs
@@ -1,7 +1,29 @@
-macro_rules! assert_delta {
-    ($x:expr, $y:expr, $d:expr) => {
-        if !(($x - $y).abs() < $d) {
-            panic!();
-        }
-    };
+#[macro_export]
+macro_rules! assert_approx_eq {
+    ($a:expr, $b:expr) => {{
+        let eps = 1.0e-6;
+        let (a, b) = (&$a, &$b);
+        assert!(
+            (*a - *b).abs() < eps,
+            "assertion failed: `(left !== right)` \
+             (left: `{:?}`, right: `{:?}`, expect diff: `{:?}`, real diff: `{:?}`)",
+            *a,
+            *b,
+            eps,
+            (*a - *b).abs()
+        );
+    }};
+    ($a:expr, $b:expr, $eps:expr) => {{
+        let (a, b) = (&$a, &$b);
+        let eps = $eps;
+        assert!(
+            (*a - *b).abs() < eps,
+            "assertion failed: `(left !== right)` \
+             (left: `{:?}`, right: `{:?}`, expect diff: `{:?}`, real diff: `{:?}`)",
+            *a,
+            *b,
+            eps,
+            (*a - *b).abs()
+        );
+    }};
 }
--- oxide_core/test/vector2.rs
+++ oxide_core/test/vector2.rs
@@ -6,14 +6,14 @@ mod test {
     fn get_squared_length() {
         let new_vector = vector2::Vector2 { x: 3.0, y: 4.0 };
         let res: f32 = 25.0;
-        assert_delta!(res, new_vector.get_squared_length(), f32::EPSILON);
+        assert_approx_eq!(res, new_vector.get_squared_length());
     }
 
     #[test]
     fn get_length() {
         let new_vector = vector2::Vector2 { x: 3.0, y: 4.0 };
         let res: f32 = 5.0;
-        assert_delta!(res, new_vector.get_length(), f32::EPSILON);
+        assert_approx_eq!(res, new_vector.get_length());
     }
 
     #[test]
@@ -21,7 +21,7 @@ mod test {
         let new_vector_1 = vector2::Vector2 { x: 3.0, y: 4.0 };
         let new_vector_2 = vector2::Vector2 { x: 3.0, y: 7.0 };
         let res: f32 = 37.0;
-        assert_delta!(res, new_vector_1.inner_product(&new_vector_2), f32::EPSILON);
+        assert_approx_eq!(res, new_vector_1.inner_product(&new_vector_2));
     }
 
     #[test]
