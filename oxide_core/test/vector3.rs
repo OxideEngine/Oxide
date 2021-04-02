6b517dd104b7560a6e970510ddf529706026a074
BeLeap <changseo_jang@korea.ac.kr>
2021-04-02 17:20:15 +0900

    test get squared length for vector3

--- oxide_core/test/vector3.rs
+++ oxide_core/test/vector3.rs
@@ -3,6 +3,17 @@ mod test {
     use crate::vector::*;
 
     #[test]
+    fn get_squared_length() {
+        let new_vector = vector3::Vector3 {
+            x: 3.0,
+            y: 4.0,
+            z: 5.0,
+        };
+        let res = 50.0;
+        assert_eq!(res, new_vector.get_squared_length());
+    }
+
+    #[test]
     fn get_length() {
         let new_vector = vector3::Vector3 {
             x: 3.0,
