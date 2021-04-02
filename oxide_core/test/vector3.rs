58d4cf931af699cd28aee48e49ca79822b5a0455
BeLeap <changseo_jang@korea.ac.kr>
2021-04-02 17:25:56 +0900

    add inner product test for vector3

--- oxide_core/test/vector3.rs
+++ oxide_core/test/vector3.rs
@@ -23,4 +23,20 @@ mod test {
         let res = 7.0710678118654752440084436210485;
         assert_eq!(res, new_vector.get_length());
     }
+
+    #[test]
+    fn inner_product() {
+        let new_vector_1 = vector3::Vector3 {
+            x: 3.0,
+            y: 4.0,
+            z: 5.0,
+        };
+        let new_vector_2 = vector3::Vector3 {
+            x: 3.0,
+            y: 7.0,
+            z: 11.0,
+        };
+        let res = 92.0;
+        assert_eq!(res, new_vector_1.inner_product(new_vector_2));
+    }
 }
