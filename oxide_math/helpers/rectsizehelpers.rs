44587462b292e01f9e48befe6f8199cec44b54e8
2019027001 <artichoke42@naver.com>
2022-03-30 14:30:44 +0900

    fix notation #3

--- oxide_math/helpers/rectsizehelpers.rs
+++ oxide_math/helpers/rectsizehelpers.rs
@@ -1,7 +1,7 @@
 use crate::components::rect::*;
 use legion::*;
 
-pub fn set_rect_size(world: &mut World, entity: &Entity, width: f32, height: f32){
+pub fn set_rect_size(world: &mut World, entity: &Entity, width: f32, height: f32) {
     assert!(width > 0.0f32 && height > 0.0f32);
     if let Some(mut entry) = world.entry(*entity) {
         entry.add_component(Rect {
