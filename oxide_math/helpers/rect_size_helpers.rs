a7bc2157849d58a94f32894ad575a89011ab136b
2019027001 <artichoke42@naver.com>
2022-04-04 00:24:09 +0900

    fix: apply requested review & remove unused import

--- oxide_math/helpers/rect_size_helpers.rs
+++ oxide_math/helpers/rect_size_helpers.rs
@@ -3,10 +3,8 @@ use legion::*;
 
 pub fn set_rect_size(world: &mut World, entity: &Entity, width: f32, height: f32) {
     assert!(width > 0.0f32 && height > 0.0f32);
+
     if let Some(mut entry) = world.entry(*entity) {
-        entry.add_component(Rect {
-            width: width,
-            height: height,
-        });
+        entry.add_component(Rect { width, height });
     }
 }
