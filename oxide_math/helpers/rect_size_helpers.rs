2e8634d38cc6bd3496088a22eeacebc007bb8d6a
2019027001 <artichoke42@naver.com>
2022-04-05 11:10:30 +0900

    feat: add logic that checks if Rect component exists in entity

--- oxide_math/helpers/rect_size_helpers.rs
+++ oxide_math/helpers/rect_size_helpers.rs
@@ -5,6 +5,12 @@ pub fn set_rect_size(world: &mut World, entity: &Entity, width: f32, height: f32
     assert!(width > 0.0f32 && height > 0.0f32);
 
     if let Some(mut entry) = world.entry(*entity) {
-        entry.add_component(Rect { width, height });
+        match entry.get_component_mut::<Rect>() {
+            Ok(comp) => {
+                comp.width = width;
+                comp.height = height;
+            }
+            Err(_) => panic!("Rect component does not exist"),
+        }
     }
 }
