ada14ebcdb01f461674928289b7edaff8b5ee0a1
2019027001 <artichoke42@naver.com>
2022-04-14 20:37:16 +0900

    test: add rect component & size helper tests

--- oxide_math/test/rect.rs
+++ oxide_math/test/rect.rs
@@ -37,4 +37,55 @@ mod test {
             assert_approx_eq!(rect.height, 3.0f32);
         }
     }
+
+    #[test]
+    #[should_panic]
+    fn wrong_width() {
+        let mut world = World::default();
+        let rect_entity: Entity = world.push((Rect {
+            width: 1.0f32,
+            height: 1.0f32,
+        },));
+        set_rect_size(&mut world, &rect_entity, -2.0f32, 3.0f32);
+
+        if let Some(entry) = world.entry(rect_entity) {
+            let rect: Rect = *entry.get_component::<Rect>().unwrap();
+            assert_approx_eq!(rect.width, -2.0f32);
+            assert_approx_eq!(rect.height, 3.0f32);
+        }
+    }
+
+    #[test]
+    #[should_panic]
+    fn wrong_height() {
+        let mut world = World::default();
+        let rect_entity: Entity = world.push((Rect {
+            width: 1.0f32,
+            height: 1.0f32,
+        },));
+        set_rect_size(&mut world, &rect_entity, 2.0f32, -3.0f32);
+
+        if let Some(entry) = world.entry(rect_entity) {
+            let rect: Rect = *entry.get_component::<Rect>().unwrap();
+            assert_approx_eq!(rect.width, 2.0f32);
+            assert_approx_eq!(rect.height, -3.0f32);
+        }
+    }
+
+    #[test]
+    #[should_panic]
+    fn wrong_width_and_height() {
+        let mut world = World::default();
+        let rect_entity: Entity = world.push((Rect {
+            width: 1.0f32,
+            height: 1.0f32,
+        },));
+        set_rect_size(&mut world, &rect_entity, -2.0f32, -3.0f32);
+
+        if let Some(entry) = world.entry(rect_entity) {
+            let rect: Rect = *entry.get_component::<Rect>().unwrap();
+            assert_approx_eq!(rect.width, -2.0f32);
+            assert_approx_eq!(rect.height, -3.0f32);
+        }
+    }
 }
