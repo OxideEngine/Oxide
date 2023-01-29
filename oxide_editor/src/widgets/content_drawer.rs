e5e5d8ac72ea5c5f3c350fece88502f454d9a79a
BeLeap <beleap@beleap.codes>
2023-01-29 11:52:18 +0900

    use Rc<RefCell<...>> instead of Arc<Mutex<...>>

--- oxide_editor/src/widgets/content_drawer.rs
+++ oxide_editor/src/widgets/content_drawer.rs
@@ -2,7 +2,7 @@ use std::{
     cell::RefCell,
     fs,
     path::{Path, PathBuf},
-    sync::{Arc, Mutex},
+    rc::Rc,
 };
 
 use eframe::egui::*;
@@ -13,13 +13,13 @@ struct ContentDrawerState {
 
 #[derive(Clone)]
 pub struct ContentDrawer {
-    state: Arc<Mutex<ContentDrawerState>>,
+    state: Rc<RefCell<ContentDrawerState>>,
 }
 
 impl ContentDrawer {
     pub fn new(location: PathBuf) -> Self {
         Self {
-            state: Arc::new(Mutex::new(ContentDrawerState {
+            state: Rc::new(RefCell::new(ContentDrawerState {
                 location: RefCell::new(location),
             })),
         }
@@ -36,40 +36,34 @@ impl Widget for ContentDrawer {
     fn ui(self, ui: &mut eframe::egui::Ui) -> eframe::egui::Response {
         ui.vertical(|ui| {
             ui.label("Content Drawer");
-            let state_arc = self.state.clone();
-            if let Ok(state) = state_arc.lock() {
-                ui.label(&state.location.borrow().to_str().unwrap().to_string());
+            let state = self.state.borrow();
+            let mut location = state.location.borrow_mut();
+            ui.label(&location.to_str().unwrap().to_string());
 
-                if ui.add(Button::new("<")).clicked() {
-                    let mut location = state.location.borrow_mut();
-                    if let Some(parent) = location.parent() {
-                        let foo: PathBuf = parent.to_path_buf();
-                        *location = foo;
-                    }
+            if ui.add(Button::new("<")).clicked() {
+                if let Some(parent) = location.parent() {
+                    *location = parent.to_path_buf();
                 }
             }
 
-            if let Ok(mut state) = state_arc.clone().lock() {
-                let location = &state.location;
-                let paths = fs::read_dir(location.borrow().to_str().unwrap()).unwrap();
-                ui.horizontal(|ui| {
-                    for path in paths.into_iter() {
-                        let path_buf = path.unwrap().path();
-                        let path: RefCell<&Path> = RefCell::new(path_buf.as_ref());
+            let paths = fs::read_dir(location.to_str().unwrap()).unwrap();
+            ui.horizontal(|ui| {
+                for path in paths.into_iter() {
+                    let path_buf = path.unwrap().path();
+                    let path: RefCell<&Path> = RefCell::new(path_buf.as_ref());
 
-                        if ui
-                            .add(Button::new(
-                                path.borrow().file_name().unwrap().to_str().unwrap(),
-                            ))
-                            .clicked()
-                        {
-                            if path.borrow().is_dir() {
-                                state.location = path.borrow().to_path_buf().into();
-                            }
+                    if ui
+                        .add(Button::new(
+                            path.borrow().file_name().unwrap().to_str().unwrap(),
+                        ))
+                        .clicked()
+                    {
+                        if path.borrow().is_dir() {
+                            *location = path.borrow().to_path_buf().into();
                         }
                     }
-                });
-            }
+                }
+            });
         })
         .response
     }
