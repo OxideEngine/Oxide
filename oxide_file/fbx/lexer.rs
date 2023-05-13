3201b731b4d9f405e7b9d98c49620e86cccb1631
BeLeap <beleap@beleap.dev>
2023-05-13 18:34:40 +0900

    check file magic

--- oxide_file/fbx/lexer.rs
+++ oxide_file/fbx/lexer.rs
@@ -3,6 +3,31 @@ use std::io::BufReader;
 use std::io::{self, Read};
 use std::path::Path;
 
+const FILE_MAGIC: [u8; 21] = [
+    75,  // `K`
+    97,  // `a`
+    121, // `y`
+    100, // `d`
+    97,  // `a`
+    114, // `r`
+    97,  // `a`
+    32,  // ` `
+    70,  // `F`
+    66,  // `B`
+    88,  // `X`
+    32,  // ` `
+    66,  // `B`
+    105, // `i`
+    110, // `n`
+    97,  // `a`
+    114, // `r`
+    121, // `y`
+    32,  // ` `
+    32,  // ` `
+    00,
+];
+
+#[derive(Debug)]
 pub struct Lexer {
     reader: BufReader<File>,
 }
@@ -12,10 +37,15 @@ impl Lexer {
         let file = File::open(target_filename)?;
         let mut reader = BufReader::new(file);
 
-        let mut buf = [0; 20];
+        let mut buf = [0; 21];
         reader.read_exact(&mut buf)?;
 
-        println!("{:?}", buf);
+        if FILE_MAGIC != buf {
+            return Err(io::Error::new(
+                io::ErrorKind::InvalidData,
+                "unmatched FILE_MAGIC",
+            ));
+        }
 
         Ok(Lexer { reader })
     }
--- oxide_file/test/fbx/lexer.rs
+++ oxide_file/test/fbx/lexer.rs
@@ -1,7 +1,19 @@
 use crate::fbx::lexer;
-use std::path::Path;
+use std::{io, path::Path};
 
 #[test]
 fn foobar() {
     let _lexer = lexer::Lexer::new(Path::new("testdata/Cube.fbx"));
+}
+
+#[test]
+fn it_should_reject_non_fbx() {
+    let lexer = lexer::Lexer::new(Path::new("testdata/Malformed.fbx"));
+
+    match lexer {
+        Ok(_) => assert!(false, "it should reject malformed fbx file"),
+        Err(e) => {
+            assert_eq!(io::ErrorKind::InvalidData, e.kind());
+        }
+    };
 }
