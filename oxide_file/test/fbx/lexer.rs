12db48c45a089d50e996fe8b0f646a00a65ec133
BeLeap <beleap@beleap.dev>
2023-05-15 12:37:37 +0900

    test: change order

--- oxide_file/test/fbx/lexer.rs
+++ oxide_file/test/fbx/lexer.rs
@@ -4,22 +4,22 @@ mod lexer {
         use std::{io, path::Path};
 
         #[test]
-        fn it_should_reject_non_fbx() {
-            let lexer = lexer::Lexer::new(Path::new("testdata/fbx/MalformedFileMagic.fbx"));
-
-            match lexer {
-                Ok(_) => assert!(false, "it should reject malformed fbx file"),
-                Err(e) => assert_eq!(io::ErrorKind::InvalidData, e.kind()),
-            };
-        }
-
-        #[test]
         fn it_should_accept_fbx() {
             let lexer = lexer::Lexer::new(Path::new("testdata/fbx/Cube.fbx"));
 
             match lexer {
                 Ok(_) => {}
                 Err(_) => assert!(false, "it should accept fbx file"),
+            };
+        }
+
+        #[test]
+        fn it_should_reject_malformed_file_magic() {
+            let lexer = lexer::Lexer::new(Path::new("testdata/fbx/MalformedFileMagic.fbx"));
+
+            match lexer {
+                Ok(_) => assert!(false, "it should reject malformed fbx file"),
+                Err(e) => assert_eq!(io::ErrorKind::InvalidData, e.kind()),
             };
         }
     }
