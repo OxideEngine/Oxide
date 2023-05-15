21f75a7001aa10eef4c85d8ad993bf70baf9981d
BeLeap <beleap@beleap.dev>
2023-05-15 12:46:55 +0900

    prepare testdata

---
+++ oxide_file/testdata/fbx/scripts/withoutVersionNumberFbx.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3
+
+import constants
+
+def main():
+    content = bytearray()
+    content.extend(constants.FILE_MAGIC)
+    content.extend(constants.NULL)
+    content.extend(constants.UNKNOWN_BYTES)
+    
+    f = open("WithoutVersionNumber.fbx", "wb")
+    f.write(content)
+
+if __name__ == "__main__":
+    main()
