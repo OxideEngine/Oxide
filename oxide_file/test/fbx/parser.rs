f039540e34313e8cd54d74886ee9cf42004f90db
BeLeap <beleap@beleap.dev>
2023-07-22 15:52:50 +0900

    rename lexer to parser

--- oxide_file/test/fbx/parser.rs
+++ oxide_file/test/fbx/parser.rs
@@ -1,13 +1,13 @@
-mod lexer {
+mod parser {
     mod new {
-        use crate::fbx::lexer;
+        use crate::fbx::parser;
         use std::{io, path::Path};
 
         #[test]
         fn it_should_accept_fbx() {
-            let lexer = lexer::Lexer::new(Path::new("testdata/fbx/Cube.fbx"));
+            let parser = parser::Parser::new(Path::new("testdata/fbx/Cube.fbx"));
 
-            match lexer {
+            match parser {
                 Ok(_) => {}
                 Err(_) => assert!(false, "it should accept fbx file"),
             };
@@ -15,9 +15,9 @@ mod lexer {
 
         #[test]
         fn it_should_reject_malformed_file_magic() {
-            let lexer = lexer::Lexer::new(Path::new("testdata/fbx/MalformedFileMagic.fbx"));
+            let parser = parser::Parser::new(Path::new("testdata/fbx/MalformedFileMagic.fbx"));
 
-            match lexer {
+            match parser {
                 Ok(_) => assert!(false, "it should reject malformed fbx file"),
                 Err(e) => assert_eq!(io::ErrorKind::InvalidData, e.kind()),
             };
@@ -25,9 +25,9 @@ mod lexer {
 
         #[test]
         fn it_should_reject_malformed_unknown_bytes() {
-            let lexer = lexer::Lexer::new(Path::new("testdata/fbx/MalformedUnknownByte.fbx"));
+            let parser = parser::Parser::new(Path::new("testdata/fbx/MalformedUnknownByte.fbx"));
 
-            match lexer {
+            match parser {
                 Ok(_) => assert!(false, "it should reject malformed unknown byte fbx file"),
                 Err(e) => assert_eq!(io::ErrorKind::InvalidData, e.kind()),
             }
@@ -35,9 +35,9 @@ mod lexer {
 
         #[test]
         fn it_should_reject_without_version_number() {
-            let lexer = lexer::Lexer::new(Path::new("testdata/fbx/WithoutVersionNumber.fbx"));
+            let parser = parser::Parser::new(Path::new("testdata/fbx/WithoutVersionNumber.fbx"));
 
-            match lexer {
+            match parser {
                 Ok(_) => assert!(false, "it should reject fbx file without version number"),
                 // Caveat: Read a file without a version field and check for EOF errors to confirm
                 // that we are consuming the version field.
